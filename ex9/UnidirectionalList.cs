044bb2255f89694e77f852b4d2dc55a171dac544
jancuz <jancuz@yandex.ru>
2018-05-31 19:16:42 +0500

    Создан класс для определения однонаправленного списка

---
+++ ex9/UnidirectionalList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex9
+{
+    //определение однонаправленного списка
+    class UnidirectionalList
+    {
+        public int data;//информационное поле
+        public UnidirectionalList next;//адресное поле
+        public UnidirectionalList()//конструктор без параметров
+        {
+            data = 0;
+            next = null;
+        }
+        public UnidirectionalList(int d)//конструктор с параметрами
+        {
+            data = d;
+            next = null;
+        }
+        public override string ToString()
+        {
+            return data + " ";
+        }
+    }
+}
